138f355fbb4e42a4087ff8c9a291a8299dc57467
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-01-03 23:52:54 +0100

    ES6 Basics

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,3 +1,4 @@
 export default function createReportObject(employeesList) {
-
+  const getNumberOfDepartments = (obj) => Object.keys(obj).length;
+  return { allEmployees: employeesList, getNumberOfDepartments };
 }
