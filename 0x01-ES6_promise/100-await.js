9d279d723634516e0e5ac568ef50eaa120647df3
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-01-04 01:20:33 +0100

    ES6 Promises

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from "./utils";
+
+export default async function asyncUploadUser() {
+  let responseData = {};
+
+  try {
+    const photoData = await uploadPhoto();
+    const userData = await createUser();
+
+    responseData = { photoData, userData };
+  } catch (error) {
+    responseData = { photoData: null, userData: null };
+  }
+
+  return responseData;
+}
