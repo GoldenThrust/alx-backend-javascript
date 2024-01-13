bc4da852c95ad1d4e0421f224b1d547159eef7f8
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-01-13 12:45:21 +0100

    Student

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,28 @@
+interface Student {
+    firstName: string,
+    lastName: string,
+    age: number,
+    location: string
+}
+
+const Student1: Student = {
+    firstName: "John",
+    lastName: "Paul",
+    age: 18,
+    location: "London"
+}
+
+const Student2: Student = {
+    firstName: "Robert",
+    lastName: "Leo",
+    age: 19,
+    location: "New York"
+}
+
+const studentsList: Array<Student> = [Student1, Student2]
+
+function createTabularStudent(students: Array<Student>): void {
+    for (const student in students) {
+        console.log(student)
+    }
+}
