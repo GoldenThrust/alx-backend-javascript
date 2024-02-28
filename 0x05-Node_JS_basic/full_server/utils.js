286598271080166a6924531bf474d49ce6780cc7
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-02-28 04:43:33 +0100

    server

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,31 @@
+import fs from "fs";
+
+function readDatabase(path) {
+  return new Promise(function (resolve, reject) {
+    if (!fs.existsSync(path)) {
+      throw new Error("Cannot load the database");
+    }
+
+    fs.readFile(path, "utf8", (err, res) => {
+      if (err) {
+        reject(new Error("Cannot load the database"));
+      }
+      const data = res.trim().split("\n");
+      const studentGroup = {};
+      for (const line of data.slice(1)) {
+        if (line.trim() !== "") {
+          const columns = line.split(",");
+          const field = columns[3];
+          const firstname = columns[0];
+          if (!(field in studentGroup)) {
+            studentGroup[field] = [];
+          }
+          studentGroup[field] = [...studentGroup[field], firstname];
+        }
+      }
+      resolve(studentGroup)
+    });
+  });
+}
+
+export default readDatabase;
