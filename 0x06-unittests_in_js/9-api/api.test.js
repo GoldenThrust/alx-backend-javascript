f0347319cf5a06374d8711a3e5d41c71e5c4972a
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-03-02 00:56:29 +0100

    CART API

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,29 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('API test', () => {
+  const BASE_URL = 'http://localhost:7865';
+
+  it('GET / returns correct response', (done) => {
+    request.get(`${BASE_URL}/`, (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns correct response when :id is a number', (done) => {
+    request.get(`${BASE_URL}/cart/47`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 47');
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns 404 response when :id is NOT a number', (done) => {
+    request.get(`${BASE_URL}/cart/HELLO_WORLD`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+});
