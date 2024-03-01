7420ce5d557a8adb9df3037954978c1c827ff6d6
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-03-02 00:50:22 +0100

    api test

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,14 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('API test', () => {
+  const BASE_URL = 'http://localhost:7865';
+
+  it('GET / returns correct response', (done) => {
+    request.get(`${BASE_URL}/`, (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
