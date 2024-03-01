cbe16c1accaa53eb8cc0510e63d5b6b0b98e7ebd
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-03-02 00:11:46 +0100

    Cheese

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,6 +1,7 @@
 const sinon = require('sinon');
-const Utils = require('./utils.js');
-const sendPaymentRequestToApi = require('./4-payment.js');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
   it('send a payment request to the API', () => {
@@ -8,10 +9,10 @@ describe('sendPaymentRequestToApi', () => {
     const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
 
     sendPaymentRequestToApi(100, 20);
-
-    sinon.assert.calledWith(stub, 'SUM', 100, 20);
-    sinon.assert.calledWith(spy, 'The total is: 10');
-
+    expect(stub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(stub.calledOnce).to.be.true;
+    expect(spy.calledWith('The total is: 10')).to.be.true;
+    expect(spy.calledOnce).to.be.true;
     spy.restore();
     stub.restore();
   });
